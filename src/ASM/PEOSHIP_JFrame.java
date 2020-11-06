ad006e8a7d3028ff155dc1e43adaa91c179fcfc7
hoang <hoang@DESKTOP-N1HLM9C>
2020-11-05 23:59:46 -0500

    test commit

--- src/ASM/PEOSHIP_JFrame.java
+++ src/ASM/PEOSHIP_JFrame.java
@@ -1,6 +1,10 @@
-/*
-
- */
+/**
+	File name: IST261_project
+	Short description: PEOSHIP_Management class
+	IST 261 Assignment: PEOSHIP MANAGERMENT
+	@author Hoang Phan
+	Oct 1st, 2020
+*/
 package ASM;
 
 import java.io.FileInputStream;
